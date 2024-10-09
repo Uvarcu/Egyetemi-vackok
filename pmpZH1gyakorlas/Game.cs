d36cc1d4157f0976299ca62e20e2960b2a0c71c5
Szabó Richárd József <113445983+Uvarcu@users.noreply.github.com>
2024-10-09 09:17:34 +0200

    Uploaded the new pmpZH1 with class usage

---
+++ pmpZH1gyakorlas/Game.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pmpZH1gyak
+{
+    internal class Game
+    {
+        public string Title;
+        public int GenreId;
+        public string Genre;
+        public string Publisher;
+        public DateTime PlatformRelease;
+        public DateTime OriginalRelease;
+        public Game(string row)
+        {
+            string[] parts = row.Split(';');
+            Title = parts[0];
+            GenreId = int.Parse(parts[1]);
+            Genre = parts[2];
+            Publisher = parts[3];
+            PlatformRelease = DateTime.Parse(parts[4]);
+            OriginalRelease = DateTime.Parse(parts[5]);
+        }
+
+        public void GenreCorrection(string[] genres)
+        {
+            GenreId = Array.IndexOf(genres, Genre);
+        }
+    }
+}
---
+++ pmpZH1gyakorlas/pmpZH1gyak.cs
@@ -0,0 +1,48 @@
+﻿namespace pmpZH1gyak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Feladat:
+            string[] availableGenres = File.ReadAllText("genre.txt").Split(", ");
+            //2.Feladat a):
+            string[] rows = File.ReadAllLines("games_dataset.csv");
+            List<Game> games = new List<Game>();
+            foreach (string row in rows)
+                if (row != rows[0]) games.Add(new Game(row));
+            //2.Feladat b):
+            foreach (Game game in games)
+                game.GenreCorrection(availableGenres);
+
+            //3.Feladat:
+            Console.WriteLine("Adj meg egy kiadót!");
+            string askedPublisher = Console.ReadLine();
+            int gamesFromThePublisher = 0;
+            foreach (Game game in games)
+                if (game.Publisher == askedPublisher) gamesFromThePublisher++;
+            Console.WriteLine($"Ettől a kiadótól: {askedPublisher} ennyi játék készült: {gamesFromThePublisher}");
+            //4.Feladat:
+            Console.WriteLine("A megjelenés napjától elérhető játékok:");
+            foreach (Game game in games)
+            {
+                if (game.PlatformRelease.Year == game.OriginalRelease.Year)
+                {
+                    Console.WriteLine($"\n\tCím: {game.Title}");
+                    Console.WriteLine($"\tMűfaj: {game.Genre}");
+                    Console.WriteLine($"\tMegjelenés éve: {game.OriginalRelease.Year}");
+                }
+            }
+            //5.Feladat:
+            Console.WriteLine("\nJátékok száma műfajonként:");
+            foreach (string genre in availableGenres)
+            {
+                int genreCount = 0;
+                foreach (Game game in games)
+                    if (game.Genre == genre) genreCount++;
+                Console.WriteLine($"\t{genre}: {genreCount} db");
+            }
+            Console.ReadKey();
+        }
+    }
+}
